8ae9e8630bfe601c8eb7ab534b2094e0c671ebf6
Marcos Paulo Pereira <mpbc315@gmail.com>
2020-12-15 13:38:27 -0300

    BoardException e programação defensiva

---
+++ src/boardgame/BoardException.java
@@ -0,0 +1,12 @@
+package boardgame;
+
+/**
+ *
+ * @author Marcos Paulo
+ */
+public class BoardException extends RuntimeException{
+    
+    public BoardException(String msg){
+        super(msg);
+    }
+}
--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -35,7 +35,7 @@ public class ChessMatch {
      */
     private void initialSetup() {
         board.placePiece(new Rook (Color.WHITE, this.board), new Position(2,1));
-        board.placePiece(new King (Color.BLACK, this.board), new Position(0,4));
+        board.placePiece(new King (Color.BLACK, this.board), new Position(4,4));
         board.placePiece(new King (Color.WHITE, this.board), new Position(7,4));
     }
 }
