c05ac8b77b72dd7a8b09dda0e59c9dd0bf69d0f7
Marcos Paulo Pereira <mpbc315@gmail.com>
2020-12-15 16:10:41 -0300

    Movendo peças

--- src/application/UI.java
+++ src/application/UI.java
@@ -1,8 +1,9 @@
 package application;
 
 import chess.ChessPiece;
-import java.util.Optional;
-import java.util.function.Consumer;
+import chess.ChessPosition;
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
 /**
  *
@@ -28,6 +29,17 @@ public class UI {
     public static final String ANSI_PURPLE_BACKGROUND = "\u001B[45m";
     public static final String ANSI_CYAN_BACKGROUND = "\u001B[46m";
     public static final String ANSI_WHITE_BACKGROUND = "\u001B[47m";
+
+    public static ChessPosition readChessPosition(Scanner sc) {
+        try {
+            String s = sc.next();
+            char column = s.charAt(0);
+            int row = Integer.parseInt(s.substring(1));
+            return new ChessPosition(column, row);
+        } catch (RuntimeException e) {
+            throw new InputMismatchException("Erro lendo ChessPosition: Valores válidos são de a1 até h8.");
+        }
+    }
 
     public static void printBoard(ChessPiece[][] pieces) {
         for (int i = 0; i < pieces[0].length; i++) {
--- src/chess/ChessException.java
+++ src/chess/ChessException.java
@@ -1,10 +1,12 @@
 package chess;
 
+import boardgame.BoardException;
+
 /**
  *
  * @author Marcos Paulo
  */
-public class ChessException extends RuntimeException {
+public class ChessException extends BoardException {
 
     public ChessException(String msg) {
         super(msg);
