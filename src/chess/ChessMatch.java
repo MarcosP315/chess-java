e2c44786b3fffdb79ceec9869431ad1d71d7e364
Marcos Paulo Pereira <mpbc315@gmail.com>
2020-12-15 14:50:04 -0300

    Pequena melhoria na impressão do tabuleiro

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -34,9 +34,19 @@ public class ChessMatch {
      * tabuleiro.
      */
     private void initialSetup() {
-        placeNewPiece('b', 6, new Rook (Color.WHITE, this.board));
-        placeNewPiece('e', 8, new King (Color.BLACK, this.board));
-        placeNewPiece('e', 1, new King (Color.WHITE, this.board));
+        placeNewPiece('c', 1, new Rook(board, Color.WHITE));
+        placeNewPiece('c', 2, new Rook(board, Color.WHITE));
+        placeNewPiece('d', 2, new Rook(board, Color.WHITE));
+        placeNewPiece('e', 2, new Rook(board, Color.WHITE));
+        placeNewPiece('e', 1, new Rook(board, Color.WHITE));
+        placeNewPiece('d', 1, new King(board, Color.WHITE));
+
+        placeNewPiece('c', 7, new Rook(board, Color.BLACK));
+        placeNewPiece('c', 8, new Rook(board, Color.BLACK));
+        placeNewPiece('d', 7, new Rook(board, Color.BLACK));
+        placeNewPiece('e', 7, new Rook(board, Color.BLACK));
+        placeNewPiece('e', 8, new Rook(board, Color.BLACK)); 
+        placeNewPiece('d', 8, new King(board, Color.BLACK));
     }
     
     private void placeNewPiece(char column, int row, ChessPiece piece){
