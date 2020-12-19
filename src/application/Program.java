2aa3de58853874dd633a0f38b6bc07efffedd95a
Marcos Paulo Pereira <mpbc315@gmail.com>
2020-12-19 20:04:07 -0300

    Fix: tratamento em Promotion

--- src/application/Program.java
+++ src/application/Program.java
@@ -18,9 +18,9 @@ public class Program {
 
     public static void main(String[] args) {
         ChessMatch chessMatch = new ChessMatch();
-        Scanner sc = new Scanner(System.in); 
+        Scanner sc = new Scanner(System.in);
         List<ChessPiece> captured = new ArrayList();
-        
+
         while (!chessMatch.getCheckMate()) {
             try {
 //                sc = new Scanner(System.in);
@@ -39,18 +39,24 @@ public class Program {
                 ChessPosition target = UI.readChessPosition(sc);
 
                 Optional.ofNullable(chessMatch.performChessMove(source, target)).ifPresent(p -> captured.add(p));
-                
+
                 if (chessMatch.getPromoted() != null) {
                     System.out.print("Digite a peça para promoção (B, N, R ou Q): ");
-                    chessMatch.replacePromotedPiece(sc.nextLine());
+                    String type = sc.nextLine().toUpperCase();
+                    while ("BNRQ".contains(type)) {
+                        System.out.println("Valor inválido! Digite novamente.");
+                        type = sc.nextLine().toUpperCase();
+                    }
+                    chessMatch.replacePromotedPiece(type);
+
                 }
-                
+
             } catch (ChessException | InputMismatchException ex) {
                 System.out.println(ex.getMessage());
                 sc.nextLine();
             }
         }
-        
+
         UI.clearScreen();
         UI.printMatch(chessMatch, captured);
     }
--- test/Teste.java
+++
@@ -1,9 +0,0 @@
-/**
- *
- * @author Marcos Paulo
- */
-public class Teste {
-    public static void main(String[] args) {
-        System.out.println("QKJM".contains("Q"));
-    }
-}
